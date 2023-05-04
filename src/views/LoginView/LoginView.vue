8b43e9ef89ac43402b458b7d148d2b02704c96d3
张伟 <zhangw@tongfun.com>
2023-05-04 17:19:05 +0800

    fix:登陆校验

--- src/views/LoginView/LoginView.vue
+++ src/views/LoginView/LoginView.vue
@@ -2,21 +2,21 @@
   <div style="position: relative;width: 100%;">
     <container-flex >
       <!--      <widget-three ref="three"/>-->
-      <login-three-bgc/>
+      <!--      <login-three-bgc/>-->
     </container-flex>
     <container-flex style="position: absolute;top: 0;pointer-events: none" :justify-content="'center'" align-items="center">
       <el-card class="box-card" style="pointer-events: auto" >
         <div class="login-box">
           <div class="login-box-title">登录</div>
-          <el-form label-width="80px"  :model="formData" ref="formRef">
+          <el-form label-width="80px" :rules="rules" :model="formData" ref="formRef">
             <el-form-item label="用户名" prop="userName" >
-              <el-input v-model="formData.userName" placeholder="请输入账号"/>
+              <el-input v-model="formData.userName" placeholder="请输入账号" @keydown.enter.native="handleKeydown"/>
             </el-form-item>
             <el-form-item label="密码" prop="password" >
-              <el-input v-model="formData.password"  placeholder="请输入密码"/>
+              <el-input v-model="formData.password"  placeholder="请输入密码" @keydown.enter.native="handleKeydown"/>
             </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="submit">登录</el-button>
+              <el-button type="primary" @click="handleKeydown">登录</el-button>
             </el-form-item>
           </el-form>
         </div>
@@ -36,7 +36,7 @@ import { getUserInfo, login } from '@/api';
 export default {
   components: {  ContainerFlex,
     // 异步加载组件，优化首次打开的时间
-    LoginThreeBgc: ()=>import('@/views/LoginView/components/login-three-bgc.vue')
+    // LoginThreeBgc: ()=>import('@/views/LoginView/components/login-three-bgc.vue')
 
   },
   computed: {
@@ -45,6 +45,10 @@ export default {
   },
   data: ()=>{
     return{
+      rules: {
+        userName: { required: true, message: '用户名必填', trigger: 'change' },
+        password: { required: true, message: '密码必填', trigger: 'change' },
+      },
       formData: {
         userName: '',
         password: ''
@@ -58,7 +62,7 @@ export default {
     async submit(){
       let res = await login(this.formData)
       console.log(res);
-      let {code,data} = res
+      let {code,msg,data} = res
       if(code === 200){
         this.setToken(data.token)
         this.setIsLogin(true)
@@ -70,9 +74,23 @@ export default {
         }
         this.$router.push('/')
       }
-      let resData = await getUserInfo()
-      console.log(resData);
+      this.$message.error(msg)
+      // let resData = await getUserInfo()
+      // console.log(resData);
       // this.setIsLogin(true)
+    },
+    handleKeydown(){
+      this.$refs.formRef.validate((isValid,fields)=>{
+        console.log(isValid,fields);
+        if(isValid){
+          this.submit()
+          return
+        }
+        let notValidList = this.$refs.formRef.$slots.default.filter(e=>e.componentOptions.propsData.prop in fields)
+        notValidList[0].componentInstance.$children[1].focus()
+        console.log(notValidList);
+        console.log(this.$refs.formRef);
+      })
     }
   }
 
